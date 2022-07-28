be13b610c02886584e9474aae8c12d844e277c4d
MikhailMS <1488maiklm@gmail.com>
2022-07-28 21:44:35 +0100

    Fix missing comma in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@
 
 
 #![deny(
-    dead_code
+    dead_code,
     missing_crate_level_docs,
     missing_doc_code_examples,
     missing_docs,
     rust_2018_idioms,
-    unused_imports,
+    unused_imports
 )]
 
 
